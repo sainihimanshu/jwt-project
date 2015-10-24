87b976f040edf06093f51fe6e6dd198759124812
Shivek Khurana <khuranashivek@gmail.com>
2015-10-24 20:55:29 +0530

    All except refresh in place.

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, g, request
 from response import Response
 from models import database, User
+from jose import jwt
+import json
 
 
 class JWT:
 
     def __init__(self):
-        pass
+        self.key = '4641CFE237FC92DCC3D65ECCAC937'
 
-    def encode(self):
-        pass
+    def encode(self, claims):
+        return jwt.encode(claims, self.key)
 
-    def decode(self):
-        pass
+    def decode(self, token):
+        return jwt.decode(token, self.key)
 
 app = Flask(__name__)
 
@@ -48,16 +50,18 @@ def authenticate():
     username = request.form.get('username', None)
     password = request.form.get('password', None)
 
-    if username and password:  
-        try:   
+    if username and password:
+        try:
             _user = User.get(User.username == username)
             if _user.password == password:
-                return Response().msg('cool, will get a token soon')
+                claims = {'id': _user.id, 'username': _user.username}
+                return Response().json({'msg': 'User authenticated', 'token': JWT().encode(claims)})
         except:
             pass
         return Response().msg('Username or password invalid', 400)
-    
+
     return Response().msg('Username and password are required', 400)
+
 
 @app.route('/refresh-token', methods=['POST'])
 def refresh_token():
@@ -66,12 +70,23 @@ def refresh_token():
 
 @app.route('/secure', methods=['GET'])
 def secure():
-    pass
+    token = request.args.get('token')
+    try:
+        claims = JWT().decode(token)
+        return Response().json({
+            'msg': 'You have arrived', 
+            'username': claims['username'],
+            'id': claims['id']
+        })
+    except:
+        return Response().not_allowed()
 
 
 @app.route('/insecure', methods=['GET'])
 def insecure():
-    pass
+    return Response().json({
+        'msg': 'Welcome !'
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
